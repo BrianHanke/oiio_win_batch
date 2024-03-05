54cb13f636ecaace8b074b4de2c8ec8bf4627bfd
Brian R Hanke <brianrhanke@outlook.com>
2024-03-05 11:04:45 -0500

    Added OpenColorIO support

--- oiio.bat
+++ oiio.bat
@@ -4,6 +4,7 @@
 @set EXR_ROOT=c:\source\oiio\openexr
 @set JPEG_ROOT=c:\source\oiio\libjpeg-turbo
 @set PNG_ROOT=c:\source\oiio\libpng\projects\vstudio\Release
+@set OCIO_ROOT=c:\source\oiio\OpenColorIO\release
 
 @set start_time=%date% %time%
 
@@ -24,9 +25,25 @@ del build\Release\zlib.lib
 copy c:\source\oiio\zlib\include\zconf.h c:\source\oiio\zlib\
 cd ..
 
+call git clone https://github.com/AcademySoftwareFoundation/openexr
+cd openexr
+call cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=dist -DBUILD_TESTING=OFF -DOPENEXR_BUILD_TOOLS=OFF -DOPENEXR_INSTALL_TOOLS=OFF -DOPENEXR_INSTALL_EXAMPLES=OFF -DZLIB_ROOT=%ZLIB_ROOT%
+call cmake --build build --target install --config Release
+cd ..
+
+call git clone https://github.com/AcademySoftwareFoundation/OpenColorIO.git
+cd OpenColorIO
+call cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DOCIO_BUILD_APPS=OFF -DOCIO_BUILD_NUKE=OFF -DOCIO_BUILD_PYTHON=OFF -DOCIO_BUILD_DOCS=OFF -DOCIO_BUILD_TESTS=OFF -DOCIO_BUILD_PYGLUE=OFF -DOCIO_BUILD_GPU_TESTS=OFF -DOCIO_BUILD_JAVA=OFF -DZLIB_ROOT=%ZLIB_ROOT%
+call cmake --build build --config Release --target install
+mkdir %OCIO_ROOT%
+xcopy /s "C:\Program Files (x86)\OpenColorIO" %OCIO_ROOT%
+rmdir /s /q "C:\Program Files (x86)\OpenColorIO"
+cd ..
+
 call git clone https://gitlab.com/libtiff/libtiff.git
 cd libtiff
-call cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=.
+call git checkout v4.3.0 --force
+call cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -Dlibdeflate=ON -DBUILD_SHARED_LIBS=ON -DZLIB_ROOT=%ZLIB_ROOT% -DCMAKE_INSTALL_PREFIX=%TIFF_ROOT%\dist
 call cmake --build build --target install
 cd ..
 
@@ -34,12 +51,6 @@ call git clone https://github.com/libjpeg-turbo/libjpeg-turbo
 cd libjpeg-turbo
 call cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX="."
 call cmake --build build --config Release --target install
-cd ..
-
-call git clone https://github.com/AcademySoftwareFoundation/openexr
-cd openexr
-call cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=dist -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DOPENEXR_BUILD_TOOLS=OFF -DOPENEXR_INSTALL_TOOLS=OFF -DOPENEXR_INSTALL_EXAMPLES=OFF -DZLIB_ROOT=%ZLIB_ROOT%
-call cmake --build build --target install --config Release
 cd ..
 
 call git clone https://github.com/BrianHanke/libpng.git
@@ -64,11 +75,14 @@ call git clone https://github.com/OpenImageIO/oiio.git
 cd oiio
 mkdir build
 cd build
-call cmake -DVERBOSE=ON -DCMAKE_BUILD_TYPE=Release -DBoost_USE_STATIC_LIBS=ON -DBoost_NO_WARN_NEW_VERSIONS=ON -DBoost_ROOT=%BOOST_ROOT% -DZLIB_ROOT=%ZLIB_ROOT% -DTIFF_ROOT=%TIFF_ROOT% -DOpenEXR_ROOT=%EXR_ROOT%\dist -DImath_DIR=%EXR_ROOT%\dist\lib\cmake\Imath -DJPEG_ROOT=%JPEG_ROOT% -DPNG_ROOT=%PNG_ROOT% -DUSE_PYTHON=0 -DUSE_QT=0 -DBUILD_SHARED_LIBS=0 -DLINKSTATIC=1 -DOIIO_BUILD_TESTS=0 ..
+call cmake -DVERBOSE=ON -DCMAKE_BUILD_TYPE=Release -DBoost_USE_STATIC_LIBS=ON -DBoost_NO_WARN_NEW_VERSIONS=ON -DBoost_ROOT=%BOOST_ROOT% -DZLIB_ROOT=%ZLIB_ROOT% -DTIFF_ROOT=%TIFF_ROOT%\dist -DOpenEXR_ROOT=%EXR_ROOT%\dist -DImath_DIR=%EXR_ROOT%\dist\lib\cmake\Imath -DJPEG_ROOT=%JPEG_ROOT% -DPNG_ROOT=%PNG_ROOT% -DOpenColorIO_ROOT=%OCIO_ROOT% -DUSE_PYTHON=0 -DUSE_QT=0 -DOIIO_BUILD_TESTS=0 ..
 
 mkdir c:\source\oiio\oiio\build\bin\Release
 copy c:\source\oiio\libpng\projects\vstudio\Release\bin\libpng16.dll c:\source\oiio\oiio\build\bin\Release\
 copy c:\source\oiio\zlib\build\Release\zlib.dll c:\source\oiio\oiio\build\bin\Release\
+copy C:\Source\oiio\OpenColorIO\release\bin\*.dll c:\source\oiio\oiio\build\bin\Release\
+copy C:\Source\oiio\openexr\dist\bin\*.dll c:\source\oiio\oiio\build\bin\Release\
+copy C:\Source\oiio\libtiff\dist\bin\*.dll c:\source\oiio\oiio\build\bin\Release\
 
 @set end_time=%date% %time%
 
